53792b698484dc89bba3545261aa617df5d44742
Nathaniel Singer <nathaniel@singer.cloud>
2022-07-26 02:02:03 -0700

    vulnerability and exploit fixes

--- honeypots/ecs-honeypot/vulnerable_software/vuln.c
+++ honeypots/ecs-honeypot/vulnerable_software/vuln.c
@@ -7,8 +7,17 @@ int main(int argc, char *argv[])
 {
 	char command[1023] = "";
 
-	sprintf(command, "/bin/bash %s", argv[1]);
-	system(command);
+	char success_criteria[] = ":)";
+	char user_input[100] = "";
+
+	printf(">");
+	fflush(stdout);
+    scanf("%s", user_input);
+    if (strcmp(user_input, success_criteria) == 0)
+    {
+		sprintf(command, "/bin/bash %s", argv[1]);
+		system(command);
+    }	
 	
 	return 0;
 }
